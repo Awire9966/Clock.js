d186ae508413f4e711b638443dba621dc4a0c69b
Awire9966 <65136929+Awire9966@users.noreply.github.com>
2021-08-14 11:28:52 -0400

    Main Clock Script

---
+++ clock.js
@@ -0,0 +1,48 @@
+function startTime() {
+    console.clear()
+    console.r
+    var minute = 0
+    var moth = 0
+    var d8 = 0
+    let month = 0
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let monthu = today.getMonth();
+    
+    let year  = today.getFullYear();
+    let day = today.getDate();
+   month = monthu + 1;
+    if(m < 10)
+{
+    minute = "0" + m;
+}
+else{
+  minute = m;
+
+}
+if(monthu < 10)
+{
+    moth = "0" + month;
+}
+else{
+  moth = month;
+
+}
+if(day < 10)
+{
+    d8 = "0" + day;
+}
+else{
+  d8 = day;
+
+}
+ 
+ 
+    console.log(moth + "/" + d8 + "/" + year);
+    console.log(h + ":" + minute);
+    
+   setTimeout(startTime, 1000);
+  }
+  startTime.call();
+  
